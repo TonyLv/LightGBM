044b6d68c11e9960a05be129d0209bfc060012f7
Nikita Titov <nekit94-08@mail.ru>
2020-02-01 22:28:25 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -233,6 +233,7 @@ def generate_r_docs(app):
         r-testthat=2.0.0=r351h29659fb_0 \
         cmake=3.14.0=h52cb24c_0 \
         ca-certificates=2019.11.27=0
+    /home/docs/.conda/bin/conda clean --all -y
     /home/docs/.conda/bin/conda install -q -y -n r_env -c conda-forge \
         r-pkgdown=1.3.0=r35h6115d3f_1001 \
         r-roxygen2=6.1.1=r35h0357c0b_1001
